ec7a24a7f515bdc31c8710271d3141099c12157c
Madlena Ivanova <madlena.iv@abv.bg>
2022-04-21 13:59:07 +0300

    Add project files.

---
+++ PetSpa.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PetSpa.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [StringLength(50)]
+        public string? LastName { get; set; }
+    }
+}
