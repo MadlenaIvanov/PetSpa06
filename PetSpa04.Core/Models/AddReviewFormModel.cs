0c41deaa1d77e4b098aba891863013bf18717aca
Madlena Ivanova <madlena.iv@abv.bg>
2022-04-21 22:34:17 +0300

    Add Review model + view added

--- PetSpa04.Core/Models/AddReviewFormModel.cs
+++ PetSpa04.Core/Models/AddReviewFormModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace PetSpa04.Core.Models
 
         public string Description { get; init; }
 
+        [Display(Name = "Image")]
         public string ImageUrl { get; init; }
 
         public int CategoryId { get; init; }
